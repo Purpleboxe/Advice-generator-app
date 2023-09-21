d7f7cd465242e46869a02a42af5bf1ca22deae6a
Antonio <antoniozamora21331@gmail.com>
2023-09-21 16:53:15 -0700

    Finish the final product

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+const dice = document.querySelector('.dice');
+
 function getAdvice() {
+    const adviceId = document.querySelector('.advice-id');
+    const adviceText = document.querySelector('.advice-textQ');
+
     fetch('https://api.adviceslip.com/advice').then(res => {
         res.json().then(data => {
+            adviceId.textContent = ('ADVICE #' + data.slip.id);
+            adviceText.innerHTML = data.slip.advice; 
             console.log(data.slip.advice);
             console.log(data.slip.id);
         })
@@ -8,5 +15,9 @@ function getAdvice() {
         console.log(err);
     });
 }
+
+dice.addEventListener('click', () => {
+    getAdvice();
+})
 
 getAdvice();
